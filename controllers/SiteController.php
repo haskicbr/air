9a8c0693502c301a2c80884796bfbd372d5b1f43
Haskicbr <haskicbr@gmail.com>
2018-04-10 17:40:49 +0500

    [del] remove other in controller

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -11,45 +11,6 @@ use yii\web\Controller;
 class SiteController extends Controller
 {
     /**
-     * @inheritdoc
-     */
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only'  => ['login', 'logout', 'signup'],
-                'rules' => [
-                    [
-                        'allow'   => true,
-                        'actions' => ['login', 'signup'],
-                        'roles'   => ['?'],
-                    ],
-                    [
-                        'allow'   => true,
-                        'actions' => ['logout'],
-                        'roles'   => ['@'],
-                    ],
-                ],
-            ],
-        ];
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ]
-        ];
-    }
-
-    /**
-     * Displays homepage.
-     *
      * @return string
      */
     public function actionIndex()
